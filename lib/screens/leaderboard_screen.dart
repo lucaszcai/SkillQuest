eefc5872d38a5e7bbf636637eae95b4bef3b1a2a
Andrewuwu <maosteram@gmail.com>
2020-04-11 15:40:16 -0500

    Leadworthy

--- lib/screens/leaderboard_screen.dart
+++ lib/screens/leaderboard_screen.dart
@@ -1,11 +1,1 @@
-class leaderboard extends StatefulWidget {
-  @override
-  _leaderboardState createState() => _leaderboardState();
-}
 
-class _leaderboardState extends State<leaderboard> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
