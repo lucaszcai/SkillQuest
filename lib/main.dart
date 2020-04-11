620647073d3a71a13e7b9726f9ad48d04c241af1
Andrewuwu <maosteram@gmail.com>
2020-04-11 15:19:30 -0500

    Leadworthy

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        //visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: EntryScreen(),
     );
