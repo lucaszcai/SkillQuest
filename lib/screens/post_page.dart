7515ac295d06249e1cd3b64cc057617bd8414f5f
lucaszcai <lucas.z.cai@gmail.com>
2020-04-11 19:43:55 -0500

    set up a post page UI

--- lib/screens/post_page.dart
+++ lib/screens/post_page.dart
@@ -21,6 +21,8 @@ class _PostPageState extends State<PostPage> {
 
 
   File _image;
+  TextEditingController _captionController = TextEditingController();
+  String _caption = '';
   String description="I loved trying this skill out!";
 
   void initState() {
@@ -70,6 +72,8 @@ class _PostPageState extends State<PostPage> {
       'ratings': 0,
       'datetime': DateTime.now().millisecondsSinceEpoch
     });
+
+    Navigator.pop(context);
     /*
     DocumentSnapshot snap =
     await Firestore.instance.collection('users').document(user.uid).get();
@@ -93,20 +97,74 @@ class _PostPageState extends State<PostPage> {
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.orange,
+        backgroundColor: Colors.white,
         elevation: 0.0,
         leading: BackButton(
-          color: Colors.white,
+          color: Colors.black,
         ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            iconSize: 30.0,
+            color: Colors.black,
+            onPressed: () => _submit(context),
+          ),
+        ],
       ),
-      body: FlatButton(
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              children: <Widget>[
+                GestureDetector(
+                  onTap: getImage,
+                  child: Container(
+                    height: width,
+                    width: width,
+                    color: Colors.grey[300],
+                    child: _image == null
+                        ? Icon(
+                      Icons.add_a_photo,
+                      color: Colors.white70,
+                      size: 150.0,
+                    )
+                        : Image(
+                      image: FileImage(_image),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30.0),
+                  child: TextField(
+                    controller: _captionController,
+                    style: TextStyle(fontSize: 18.0),
+                    decoration: InputDecoration(
+                      labelText: 'Caption',
+                    ),
+                    onChanged: (input) => description = input,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      )
+    );
+  }
+
+}
+
+/*
+FlatButton(
         child: Text('Take PICTURE'),
         onPressed:(){
           getImage();
           _submit(context);
         },
       ),
-    );
-  }
 
-}
+ */
