5e4ce042b26c97b9a9e8a7e455f41eac646b91ff
Andrewuwu <maosteram@gmail.com>
2020-04-11 22:51:11 -0500

    Okay I finally finished this lol

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,9 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
