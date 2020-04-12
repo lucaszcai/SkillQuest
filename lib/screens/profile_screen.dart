a6a337796149e1336f247e19ad5e4d29f0f6af1c
VincentDo8439 <33644870+VincentDo8439@users.noreply.github.com>
2020-04-11 21:39:46 -0500

    finished profile page

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -6,30 +7,27 @@ class ProfileScreen extends StatefulWidget {
   _ProfileScreenState createState() => _ProfileScreenState();
 }
 
-_toolBar() {
+/* _toolBar() {
   return Padding(
-    padding: EdgeInsets.all(32.0),
+    padding: EdgeInsets.all(20.0),
     child: Row(
       mainAxisSize: MainAxisSize.max,
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
-        Icon(
-          Icons.arrow_back_ios,
-          color: Colors.white,
-        ),
+
         Text(
           '@lucascaixizang',
           style: TextStyle(
               color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
         ),
-        Icon(
-          Icons.more_vert,
-          color: Colors.white,
-        )
+
       ],
     ),
   );
 }
+
+ */
+
 
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
@@ -45,24 +43,52 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 bottom: 0,
                 left: 0,
                 right: 0,
-                child: Image.network(
-                  "https://images.unsplash.com/photo-1527632756259-26693f831311?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80",
-                  fit: BoxFit.cover,
-                )),
-            Positioned(
-                top: 0,
-                bottom: 0,
-                left: 0,
-                right: 0,
                 child: Container(
                   decoration: BoxDecoration(
                       gradient: LinearGradient(
-                    colors: [
-                      Color(0x0041444B),
-                      Color(0x6041444B),
-                    ],
-                  )),
+                        colors: [
+                          Colors.black,
+                          Colors.black,
+                        ],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                      )),
                 )),
+
+            Positioned(
+                top: 0,
+                bottom: 300,
+                left: 0,
+                right: 0,
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage(
+                    "https://images.unsplash.com/photo-1527632756259-26693f831311?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80",
+                    )),
+                ),
+                ),
+            Positioned(
+              top: 190,
+              bottom: 280,
+              left: 100,
+              right: 0,
+              child: Padding(
+                padding: EdgeInsets.only(top: 40, bottom: 40, left: 10),
+                child: Container(
+                  width: 10,
+                  height: 10,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(12),
+                        bottomLeft: Radius.circular(12),
+                      )
+                  ),
+                ),
+              ),
+            ),
             Positioned(
               top: 20,
               bottom: 0,
@@ -72,8 +98,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 mainAxisSize: MainAxisSize.max,
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
-                  _toolBar(),
-                  _body(),
+                  //_toolBar(),
+                  Expanded(
+                  child: _body(),
+                  ),
                 ],
               ),
             )
@@ -82,6 +110,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
       ),
     );
   }
+
+
 }
 
 _body() {
@@ -93,7 +123,8 @@ _body() {
           alignment: Alignment.centerRight,
           child: _profileInfo(),
         ),
-        //_profilePosts(),
+        _profilePosts(),
+        _profileActionsAndInfo(),
       ],
     ),
   );
@@ -108,13 +139,186 @@ _profileInfo() {
       children: <Widget>[
         Text(
           "Lucas Cai",
-          style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),
+          style: TextStyle(color: Colors.black, fontSize: 40, fontWeight: FontWeight.bold),
         ),
         Text(
           "Recorder Master",
-          style: TextStyle(color: Colors.white, fontSize: 20),
+          style: TextStyle(color: Colors.black, fontSize: 20),
         ),
       ],
+    ),
+  );
+}
+
+_profilePosts() {
+  return Container(
+    width: 360,
+    height: 200,
+    child: ListView.builder(
+        itemCount: 8,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return _numberOfPosts();
+          } else if (index == 7) {
+            return _seeMorePosts();
+          } else {
+            return _profilePost(index);
+          }
+        }),
+  );
+}
+
+_profileActionsAndInfo() {
+  return Container(
+    width: 360,
+    height: 100,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        MaterialButton(
+          onPressed: () {},
+          shape: StadiumBorder(),
+          color: Colors.white,
+          child: Text(
+            "EDUCATION",
+          ),
+        ),
+        Icon(
+          Icons.school,
+          color: Colors.white,
+        ),
+        MaterialButton(
+          onPressed: () {},
+          shape: StadiumBorder(),
+          color: Colors.white,
+          child: Text(
+            "MUSIC",
+          ),
+        ),
+        Icon(
+          Icons.music_note,
+          color: Colors.white,
+        )
+      ],
+    ),
+  );
+}
+
+_numberOfPosts() {
+  return Padding(
+    padding: EdgeInsets.only(top: 45, bottom: 45, right: 10),
+    child: Container(
+      width: 85,
+      height: 110,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(12),
+            bottomRight: Radius.circular(12),
+          )),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            Text(
+              "6",
+              style: TextStyle(fontSize: 40, color: Colors.black),
+            ),
+            Text(
+              "SKILLS",
+              style: TextStyle(fontSize: 14, color: Colors.black),
+            )
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+
+_profilePost(int index) {
+  String profilePostImage = "";
+
+  switch (index) {
+    case 0:
+      profilePostImage =
+      "https://images.unsplash.com/photo-1466584241662-8cb021032c1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80";
+      break;
+    case 1:
+      profilePostImage =
+      "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80";
+      break;
+    case 2:
+      profilePostImage =
+      "https://images.unsplash.com/photo-1516641463285-bf3f40e65bae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80";
+      break;
+    case 3:
+      profilePostImage =
+      "https://images.unsplash.com/photo-1418290232843-5d7a0bd93f7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60";
+      break;
+    case 4:
+      profilePostImage =
+      "https://images.unsplash.com/photo-1581598681059-18f5871e1d48?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80";
+      break;
+    case 5:
+      profilePostImage =
+      "https://images.unsplash.com/photo-1466584241662-8cb021032c1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80";
+      break;
+    default:
+      profilePostImage =
+      "https://images.unsplash.com/photo-1585521551675-64daba4ba31e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80";
+      break;
+  }
+
+  return Padding(
+    padding: EdgeInsets.only(left: 10, right: 10),
+    child: Container(
+      width: 200,
+      height: 110,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(12))),
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+        child: Image.network(
+          profilePostImage,
+          fit: BoxFit.cover,
+        ),
+      ),
+    ),
+  );
+}
+
+_seeMorePosts() {
+  return Padding(
+    padding: EdgeInsets.only(top: 40, bottom: 40, left: 10),
+    child: Container(
+      width: 100,
+      height: 110,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(12),
+            bottomLeft: Radius.circular(12),
+          )),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(Icons.arrow_forward_ios, color: Colors.black,),
+            Text(
+              "Explore More",
+              style: TextStyle(fontSize: 16, color: Colors.black),
+            )
+          ],
+        ),
+      ),
     ),
   );
 }
